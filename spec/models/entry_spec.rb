8449703343ad35f2292b0a5cc6798b171dd50cc5
Matt Larner <mrlarner@gmail.com>
2013-11-27 02:48:43 -0800

    simplifying Entry model spec

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -3,30 +3,20 @@ require 'spec_helper'
 
 describe Entry do
   before do
-    create_list(:entry, 15)
+    FactoryGirl.create_list(:entry, 15)
   end
 
   let(:entries) { Entry.all }
   let(:latest) { Entry.latest }
   let(:latest_13) { Entry.latest(13) }
 
-  context '#all' do
-    it "orders in reverse chronology" do
-      entries.first.should be_newest_of(entries)
-    end
+  describe '#all' do
+    it { expect(entries.first).to be_newest_of(entries)}
   end
 
-  context "#latest" do
-    it "orders in reverse chronology" do
-      latest.first.should be_newest_of(latest)
-    end
-
-    it "returns 5 by default" do
-      latest.count.should == 5
-    end
-
-    it "returns the requested amount" do
-      latest_13.count.should == 13
-    end
+  describe "#latest" do
+    it { expect(latest.first).to be_newest_of(latest) }
+    it { expect(latest).to have(5).item }
+    it { expect(latest_13).to have(13).item }
   end
 end
