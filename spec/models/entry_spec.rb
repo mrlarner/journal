94ce9f0cd49c08304f187a3cb189d1808f7bf788
Matt Larner <mrlarner@gmail.com>
2013-11-27 13:18:56 -0800

    cleaned up entry spec

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,22 +1,24 @@
 require 'spec_helper'
 
-
 describe Entry do
   before do
     FactoryGirl.create_list(:entry, 15)
   end
-
-  let(:entries) { Entry.all }
-  let(:latest) { Entry.latest }
-  let(:latest_13) { Entry.latest(13) }
-
+  
   describe '#all' do
-    it { expect(entries.first).to be_newest_of(entries)}
+    subject { Entry.all }
+    it { subject.first.should be_newest_of(subject)}
   end
 
   describe "#latest" do
-    it { expect(latest.first).to be_newest_of(latest) }
-    it { expect(latest).to have(5).item }
-    it { expect(latest_13).to have(13).item }
+    subject { Entry.latest }
+    it { subject.first.should be_newest_of(subject) }
+    it { should have(5).item }
+    
+    context "specific number" do
+      let(:number) { Random.rand(1..Entry.count) }
+      subject { Entry.latest(number) }
+      it { should have(number).item }
+    end
   end
 end
