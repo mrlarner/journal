254b39cfbc87977d0081126d7c0f2bf8bf186c6b
Matt Larner <mrlarner@gmail.com>
2013-11-27 02:48:20 -0800

    initial EntriesController spec

--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
-describe EntriesController do
+describe EntriesController, type: :controller do
+  before(:each) do
+    FactoryGirl.create_list(:entry, 5)
+  end
 
+  let(:latest) { Entry.latest }
+
+  describe "GET index" do
+    it "assigns latest entries" do
+      get :index
+      expect(assigns(:entries)).to eq(latest)
+    end
+  end
 end
