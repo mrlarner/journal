019cd3a3adfd78a464ff8f910dee24c6ba01665f
Matt Larner <mrlarner@gmail.com>
2013-11-27 02:04:44 -0800

    basic entries spec for latest and default scope ordering

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
+
 describe Entry do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    create_list(:entry, 15)
+  end
+
+  let(:entries) { Entry.all }
+  let(:latest) { Entry.latest }
+  let(:latest_13) { Entry.latest(13) }
+
+  context '#all' do
+    it "orders in reverse chronology" do
+      entries.first.should be_newest_of(entries)
+    end
+  end
+
+  context "#latest" do
+    it "orders in reverse chronology" do
+      latest.first.should be_newest_of(latest)
+    end
+
+    it "returns 5 by default" do
+      latest.count.should == 5
+    end
+
+    it "returns the requested amount" do
+      latest_13.count.should == 13
+    end
+  end
 end
